5f06289548edcf7db3a70fbdd9ae3641990fc319
Augustine <augustine_theo@localhost.localdomain>
2019-03-16 04:56:40 -0400

    updated

--- contracts/Level1.sol
+++ contracts/Level1.sol
@@ -1,102 +1,179 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.5.0;
 
 contract Level1 {
     address public state_gov;
-
-    string public addr = '0x0';
-
+    
+    struct Proposal
+    {
+        
+        uint proposalId;
+        uint organizationId;
+        uint taskId;
+        
+        string proposalDescription;
+        string proposalTitle;
+        bool is_awarded;
+        
+        /*
+        uint costEstimate;
+        uint duration; // in months
+        uint epoch; // in seconds 
+        */
+        
+    }
+    
     struct Task {
-        string desc;
-        uint status;
+       uint taskId;
+       string taskTitle;
+       /*
+       string taskDescription;// a description of the task
+       */
+       
+       uint propId; // proposal chosen for this taskDescription
+       uint numVotes;
+       uint status;// (0-100) level of completion
+       bool is_completed ;
+       
+       /*
+       uint propSubmisssionDeadline;
+       uint executionDeadline;
+       */
+       uint[] taskProposalsId;// proposalId's of proposals received for the given task
+       
+       
+       
+       
     }
-
-
-    struct District {
-        address add;
-        string name;
-        uint[] prj;
-
-
-
+    
+    
+    struct Organization
+    {
+        address organizationAddress;// hash key for org
+        
+        uint organizationId;
+        string organizationTitle;
+        
+        uint[] organizationProposalsId; // proposalId's of proposals submitted by this organization
+        
+        // registerProposal(taskId)
     }
+    
 
-    struct Contracts {
-        uint taskID;
-        address distADD;
-        }
-
-    mapping(string => uint) district;
-
-    District[] public dists;
+    struct District
+    {
+        address districtAddress; // hash key for district
+        string districtTitle;
+        uint districtId;
+        
+        
+        uint[] districtTasksId;
+    }
+    
+    struct Vote
+    {
+        
+        uint taskId;
+        uint vote_points; // 0-100
+    }
+    
+    struct Citizen
+    {
+        address citizenAddress;
+        string citizenName;
+        uint citizenId;
+        
+        uint[] citizenVote; 
+    }
+    
+    
+    
+    
+    District[] public districts;
     Task[] public tasks;
-    Contracts[] public conts;
-
-
-
-    function getTaskId(uint idx) public view returns(uint)
-    {
-        return conts[idx].taskID;
-    }
-
-    function getDistAddress(uint idx) public view returns(address)
-    {
-            return conts[idx].distADD;
-    }
-
-    //Getters for Districts
-    function getAddress(uint idx) public view returns(address)
-    {
-        return dists[idx].add;
-    }
-
-    function getName(uint idx) public view returns(string)
-    {
-        return dists[idx].name;
-    }
-
-
-
-
-    // Getters for Tasks
-    function getDescription(uint idx) public view returns(string)
-    {
-        return tasks[idx].desc;
-    }
-
-    function getStatus(uint idx) public view returns (uint)
-    {
-        return tasks[idx].status;
-    }
-
+    Organization[] public organizations;
+    Proposal[] public proposals;
+    Citizen[] public citizens;
+    Vote[] public votes;
+    
+    
+    // Assumption names of entities are unique
+    mapping(string => uint) distMap;// distTitle  to index in dist with distTitle
+    mapping(string => uint) orgMap; // orgTitle to index in organizations with orgTitle 
+    mapping(string => uint) taskMap;// taskTitle to index in tasks with orgTitle
+    mapping(string => uint) propMap;// propTitle to index in proposals with propTitle
+    mapping(string => uint) citiMap;// citizenName to index in citizens with citizenName
+    //mapping()
+    
 
     constructor() public {
         state_gov = msg.sender;
     }
 
-    function addTask(string memory d) public {
+    function registerTask(uint task_id, string memory task_title) public {
         require(msg.sender == state_gov);
-        tasks.push(Task({desc: d, status: 0}));
+        uint[] memory empArr;
+        tasks.push(Task({taskId:task_id, taskTitle:task_title,numVotes:0, propId:0,status:0,is_completed: false, taskProposalsId:empArr}));
+        taskMap[task_title] = tasks.length-1;
+
     }
 
-    function registerDistrict(address a, string memory n) public {
+    function registerDistrict(address dist_address, uint dist_Id, string memory dist_Title) public {
         require(msg.sender == state_gov);
-        uint[] memory emp;
-        dists.push(District({add: a, name: n, prj: emp}));
-        district[n] = dists.length - 1;
-        addr = n;
+        uint[] memory empArr;
+        districts.push(District({districtAddress:dist_address,districtId: dist_Id, districtTitle:dist_Title,districtTasksId:empArr}));
+        distMap[dist_Title] = districts.length - 1;
+    }
+    
+    function registerOrganization(address org_address, uint org_Id, string memory org_title) public
+    {
+        require(msg.sender==state_gov);
+        uint[] memory empArr;
+        organizations.push(Organization({organizationAddress:org_address, organizationId:org_Id, organizationTitle: org_title,organizationProposalsId:empArr}));
+        
+        orgMap[org_title] = organizations.length-1;
     }
 
-    function issueContract(string memory n, uint tsk) public {
+    function registerProposal(uint prop_Id, string memory org_title,string memory task_title, string memory prop_Desc, string memory prop_Title) public {
         require(msg.sender == state_gov);
-        conts.push(Contracts({taskID: tsk, distADD: dists[district[n]].add}));
-        District storage d = dists[district[n]];
-        d.prj.push(tsk);
-        if (dists[district[n]].prj.length == 0)
-            revert();
+        uint org_Id = organizations[orgMap[org_title]].organizationId;
+        uint task_Id = tasks[taskMap[task_title]].taskId;
+        
+        proposals.push(Proposal({proposalId:prop_Id,organizationId:org_Id, taskId:task_Id, is_awarded:false, proposalDescription:prop_Desc,proposalTitle:prop_Title}));
+        propMap[prop_Title] =proposals.length - 1;
+        
+        
+        // the current proposal has to be added to the respective task ,and organization
+        organizations[orgMap[org_title]].organizationProposalsId.push(prop_Id);
+        tasks[taskMap[task_title]].taskProposalsId.push(prop_Id);
+        
     }
-
-    function getProjects(uint districtId) public view returns (uint[]) {
-            return dists[districtId].prj;
+    /*
+    function registerCitizen(address cit_address,uint cit_Id,string memory cit_Name) public
+    {
+        require(msg.sender ==state_gov);
+        
+        uint[] memory empArr;
+        citizens.push(Citizen({citizenAddress:cit_address, citizenName: cit_Name, citizenId: cit_Id,citizenVote:empArr}));
+        citiMap[cit_Name] = citizens.length-1;
     }
-
+    
+    
+    function citizenTaskVote(string memory cit_title,string memory task_title,uint vote_value) public
+    {
+        require(msg.sender==state_gov);
+        /*
+        TODO : 
+            1. Take care of case when votes are being cast for task that is not awarded yet
+        //
+        uint task_Id = tasks[taskMap[task_title]].taskId;
+        citizens[citiMap[cit_title]].citizenVote.push(Vote({taskId:task_Id,vote_points:vote_value}));
+        uint num_votes = tasks[taskMap[task_title]].numVotes;
+        tasks[taskMap[task_title]].status = uint((tasks[taskMap[task_title]].status*num_votes+vote_value)/(num_votes+1));
+        tasks[taskMap[task_title]].numVotes+=1;
+        
+    }
+    //*/
+    
+    
 }
+
